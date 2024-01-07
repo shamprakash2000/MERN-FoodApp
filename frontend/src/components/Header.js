fda572ba272c2a35dcc00fd53ae87535746e6671
shamprakash2000 <shamprakash2000@gmail.com>
2024-01-07 21:15:03 +0530

    New Product frontend completed.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,6 +5,9 @@ import { HiOutlineUserCircle } from "react-icons/hi";
 import { BsCartFill } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
 import {logoutRedux} from '../redux/userSlice'
+import toast from "react-hot-toast"
+
+
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
   const userData = useSelector((state)=>state.user)
@@ -19,6 +22,8 @@ const Header = () => {
 
   const handleLogout=()=>{
     dispatch(logoutRedux())
+    toast("Logout Successfully")
+    
 
   }
   return (
@@ -49,17 +54,17 @@ const Header = () => {
             </div>
           </div>
           <div className=" text-slate-600" onClick={handleShowMenu}>
-            <div className="text-3xl cursor-pointer  w-10 h-10 rounded-full overflow-hidden drop-shadow" >
+            <div className="text-3xl cursor-pointer  w-8 h-8 rounded-full overflow-hidden drop-shadow-md" >
               
               {userData.image? <img src={userData.image}/>:
                 <HiOutlineUserCircle />
               }
             </div>
             {showMenu && (
-              <div className="absolute right-2 bg-white py-2 px-2 shadow drop-shadow-md flex flex-col">
-                <Link to={"NewProduct"} className="whitespace-nowrap cursor-pointer">New Product</Link>
+              <div className="absolute right-2 bg-white py-2 shadow drop-shadow-md flex flex-col">
+                <Link to={"NewProduct"} className="whitespace-nowrap cursor-pointer px-2">New Product</Link>
                {
-                userData.image? <p className="cursor-pointer" onClick={handleLogout}>Logout</p>:<Link to={"Login"} className="whitespace-nowrap cursor-pointer">Login</Link>
+                userData.image? <p className="cursor-pointer px-2 bg-red-300 text-white" onClick={handleLogout}>Logout</p>:<Link to={"Login"} className="whitespace-nowrap cursor-pointer px-2">Login</Link>
                }
               </div>
             )}
--- frontend/src/page/NewProduct.js
+++ frontend/src/page/NewProduct.js
@@ -1,9 +1,79 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {BsCloudUpload} from 'react-icons/bs'
+import { ImageToBase64 } from "../utility/imaheToBase64";
 const NewProduct = () => {
+
+  const [data,setData] = useState({
+    name:"",
+    category:"",
+    image:"",
+    price:"",
+    description:""
+
+  })
+
+  const handleOnChange = (e)=>{
+      const {name,value} = e.target;
+      setData((preve)=>{
+        return{
+          ...preve,
+          [name]:value
+        }
+      })
+
+  }
+const uploadImage =async (e)=>{
+  const data = await ImageToBase64(e.target.files[0]);
+  setData((preve)=>{
+    return{
+      ...preve,
+      image:data
+    }
+  })
+  console.log(data);
+}
+
+const handleSubmit = (e)=>{
+  e.preventDefault();
+  console.log(data)
+}
+
   return (
-    <div>
-      NewProduct
+    <div className='p-4'>
+      <form className='m-auto w-full max-w-md shadow flex flex-col p-3 bg-white' onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input type={"text"} name="name" className='bg-slate-200 p-1 my-1' onChange={handleOnChange}/>
+      <label name="category">Category</label>
+      <select className='bg-slate-200 p-1 my-1 ' name="category" id='category' onChange={handleOnChange}>
+        <option>Fruits</option>
+        <option>Vegetable</option>
+        <option>Icecream</option>
+        <option>Dosa</option>
+        <option>Pizza</option>
+                
+      </select>
+
+      <label htmlFor="image">Image
+      <div className='h-40 w-full bg-slate-300 my-3 rounded flex items-center justify-center cursor-pointer'>
+        {
+          data.image?<img src={data.image} className="h-full"/>:<span className='text-5xl'><BsCloudUpload/></span>
+        }
+        
+        
+        <input type={"file"} accept='image/*' id="image" onChange={uploadImage} className='hidden'/>
+      </div>
+      </label>
+
+      <label htmlFor="price" className='my-1'>Price</label>
+      <input type={"text"} className='bg-slate-200 p-1 my-1'name="price" onChange={handleOnChange}/>
+
+      <label htmlFor="description"  className='my-1'>Description</label>
+      <textarea rows={3} className='bg-slate-200 p-1 my-1 resize-none' name="description" onChange={handleOnChange}></textarea>
+
+      <button className='bg-red-500 hover:bg-red-600 tex-white text-lg font-m drop-shadow my-1 mx-1'>Save</button>
+
+
+      </form>
     </div>
   )
 }
